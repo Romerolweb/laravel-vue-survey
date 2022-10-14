38762f37e55d15e7b0eab99d91ce47ae132a304f
Romerolweb <>
2022-10-14 08:10:27 -0500

    Csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'http://calculadorahidricagestores.com/*',
+        'http://35.171.140.220/*'
     ];
 }
